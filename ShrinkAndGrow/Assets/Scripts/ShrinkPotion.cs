e0d8366e597a3a5ddea34b329b5c1883d2fb2f35
Laia <laia.fustercorral@gmail.com>
2023-11-09 20:33:46 +0100

    Added shrinking flower sprite with a randomly activated dripping animation

--- ShrinkAndGrow/Assets/Scripts/ShrinkPotion.cs
+++ ShrinkAndGrow/Assets/Scripts/ShrinkPotion.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class ShrinkPotion : MonoBehaviour
 {
+    private Animator animator;
+    private int drippingRand;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        //Every 2 seconds we check if we should play the dripping animation
+        InvokeRepeating(nameof(CheckDripping), 2, 2);
+    }
+
+    private void CheckDripping()
+    {
+        drippingRand = Random.Range(0, 2);
+        if (drippingRand == 0)
+            animator.SetTrigger("Drip");
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         CharacterInventory invetory = collision.GetComponent<CharacterInventory>();
