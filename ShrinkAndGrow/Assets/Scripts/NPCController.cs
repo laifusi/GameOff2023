9ad1da31205320cec9e16edfe8a48d24bb1e612d
Laia <laia.fustercorral@gmail.com>
2023-11-25 21:32:49 +0100

    Added walk to destiny implementation

--- ShrinkAndGrow/Assets/Scripts/NPCController.cs
+++ ShrinkAndGrow/Assets/Scripts/NPCController.cs
@@ -4,19 +4,81 @@ using UnityEngine;
 
 public class NPCController : MonoBehaviour
 {
-    Animator animator;
+    [SerializeField] Transform[] destinies;
+    [SerializeField] float walkSpeed = 6f;
+    [SerializeField] float distanceThreshold = 0.1f;
+
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rb;
+    private int nextDestiny;
+    private bool isWalking;
+    private Transform destiny;
+    private Vector3 currentPosition;
+    private Vector3 targetPosition;
+    private Vector3 direction;
+    private bool firstContact;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if(isWalking)
+        {
+            UpdatePosition();
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.GetComponent<CharacterMovement>() != null)
+        CharacterInventory inventory = collision.GetComponent<CharacterInventory>();
+        if (inventory != null)
         {
+            if(!firstContact)
+            {
+                animator.SetTrigger("Idle");
+                firstContact = true;
+            }
+            else if(inventory.HasDiamond())
+            {
+                StartWalk();
+            }
+        }
+    }
+
+    public void StartWalk()
+    {
+        if(destinies.Length > nextDestiny)
+        {
+            animator.SetTrigger("Walk");
+            isWalking = true;
+            destiny = destinies[nextDestiny];
+            nextDestiny++;
+        }
+    }
+
+    private void UpdatePosition()
+    {
+        currentPosition = transform.position;
+        targetPosition = destiny.position;
+        if (Vector3.Distance(currentPosition, targetPosition) > distanceThreshold)
+        {
+            direction = targetPosition - currentPosition;
+            direction.Normalize();
+            spriteRenderer.flipX = direction.x < 0;
+            rb.velocity = new Vector2(walkSpeed * direction.x, rb.velocity.y);
+        }
+        else
+        {
+            isWalking = false;
+            spriteRenderer.flipX = false;
             animator.SetTrigger("Idle");
-            GetComponent<Collider2D>().enabled = false;
+            rb.velocity = Vector2.zero;
         }
     }
 }
